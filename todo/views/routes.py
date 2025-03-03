0bf9a6eb9351e908ca316091826836c9f1459ce9
Slothlesss <109386583+Slothlesss@users.noreply.github.com>
2025-03-03 21:58:30 +0000

    Fix the "id" = 1 in method that requires id

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -5,11 +5,23 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 @api.route('/health')
 def health():
     return jsonify({'status': 'ok'})
-    
+    @api.route('/todos', methods=['GET']) 
+
+def get_todos(): 
+    return jsonify([{ 
+        "id": 1, 
+        "title": "Watch CSSE6400 Lecture", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
+        "completed": True, 
+        "deadline_at": "2023-02-27T00:00:00", 
+        "created_at": "2023-02-20T00:00:00", 
+        "updated_at": "2023-02-20T00:00:00" 
+    }])
+
 @api.route('/todos/<int:id>', methods=['GET']) 
 def get_todo(id): 
     return jsonify({
-        "id": 1, 
+        "id": id, 
         "title": "Watch CSSE6400 Lecture", 
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
@@ -33,7 +45,7 @@ def create_todo():
 @api.route('/todos/<int:id>', methods=['PUT']) 
 def update_todo(id): 
     return jsonify({
-        "id": 1, 
+        "id": id, 
         "title": "Watch CSSE6400 Lecture", 
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
@@ -45,7 +57,7 @@ def update_todo(id):
 @api.route('/todos/<int:id>', methods=['DELETE']) 
 def delete_todo(id): 
     return jsonify({
-        "id": 1, 
+        "id": id, 
         "title": "Watch CSSE6400 Lecture", 
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
